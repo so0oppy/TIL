b22ebb5712dbac0bd5d11af6dde163a1f86187ce
hansb1023 <hansb102318@gmail.com>
2022-07-26 05:11:50 +0000

    22.07.26 study_BOJ 10808 (add better code)

--- Algorithm_study/BOJ 10808.cpp
+++ Algorithm_study/BOJ 10808.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -15,5 +16,23 @@ int main(){
 		}
 		cout << cnt << ' ' ;
 	}
+}
+*/
+
+// 더 나은 코드
+int freq[26];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	
+	string S;
+	cin >> S;
+	
+	for(auto c : S){
+		freq[c - 'a']++;
+	}
+	for(int i=0; i<26; i++){
+		cout << freq[i] << ' ';
+	}
 }
